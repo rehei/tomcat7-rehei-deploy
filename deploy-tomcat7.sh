ab93af10acafc7d80186560062a19bf636dcfe72
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2017-03-08 10:50:29 +0100

    Use external deployment directory to avoid redundant data in eclipse

--- deploy-tomcat7.sh
+++ deploy-tomcat7.sh
@@ -11,18 +11,24 @@ PORT=${PORT:="8080"}
 BRANCH=${BRANCH:="master"}
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
+DEPLOYMENT=$DIR/../$(basename $DIR)-deployment
+
+# CLEAN
+rm -rf $DEPLOYMENT
+
+# BUILD 
+mkdir $DEPLOYMENT
+cd $DIR 
 
 # Work with sources as managed by git 
-mkdir $DIR/deployment
-cd $DIR 
-git archive --format zip --output $DIR/deployment/sources.zip $BRANCH
-mkdir $DIR/deployment/sources
-unzip $DIR/deployment/sources.zip -d $DIR/deployment/sources/. 
+git archive --format zip --output $DEPLOYMENT/sources.zip $BRANCH
+mkdir $DEPLOYMENT/sources
+unzip $DEPLOYMENT/sources.zip -d $DEPLOYMENT/sources/. 
 
 # sbt package 
-cd $DIR/deployment/sources
+cd $DEPLOYMENT/sources
 ./sbt package
-cd $DIR/deployment/sources/target/scala-2.11/
+cd $DEPLOYMENT/sources/target/scala-2.11/
 
 # More VARIABLES
 
@@ -30,34 +36,34 @@ APP=$(ls *_2.11-${TAG}.war | sed 's/\(.*\)_2.11-.*\.war/\1/')
 TOMCAT=tomcat7-${APP}-${PORT}
 
 # Download TOMCAT 
-mkdir $DIR/deployment/tmp
-cd $DIR/deployment/tmp
+mkdir $DEPLOYMENT/tmp
+cd $DEPLOYMENT/tmp
 curl -L -J -O $URL
-unzip apache-tomcat-7.0.63-windows-x64-rehei.zip -d $DIR/deployment/${TOMCAT}
-cp $DIR/deployment/sources/target/scala-2.11/${APP}_2.11-${TAG}.war $DIR/deployment/${TOMCAT}/webapps/${CONTEXT}.war
-rm -rf $DIR/deployment/tmp
+unzip apache-tomcat-7.0.63-windows-x64-rehei.zip -d $DEPLOYMENT/${TOMCAT}
+cp $DEPLOYMENT/sources/target/scala-2.11/${APP}_2.11-${TAG}.war $DEPLOYMENT/${TOMCAT}/webapps/${CONTEXT}.war
+rm -rf $DEPLOYMENT/tmp
 
 # Copy sources to TOMCAT root 
-cp $DIR/sources.zip $DIR/deployment/tomcat7-${APP}/.
+cp $DIR/sources.zip $DEPLOYMENT/tomcat7-${APP}/.
 
 # Unpack package to TOMCAT webapps 
-mv $DIR/deployment/${TOMCAT}/webapps/${CONTEXT}.war $DIR/deployment/${TOMCAT}/webapps/${CONTEXT}.zip
-unzip $DIR/deployment/${TOMCAT}/webapps/${CONTEXT}.zip -d $DIR/deployment/${TOMCAT}/webapps/${CONTEXT}
-rm -rf $DIR/deployment/${TOMCAT}/webapps/${CONTEXT}.zip
+mv $DEPLOYMENT/${TOMCAT}/webapps/${CONTEXT}.war $DEPLOYMENT/${TOMCAT}/webapps/${CONTEXT}.zip
+unzip $DEPLOYMENT/${TOMCAT}/webapps/${CONTEXT}.zip -d $DEPLOYMENT/${TOMCAT}/webapps/${CONTEXT}
+rm -rf $DEPLOYMENT/${TOMCAT}/webapps/${CONTEXT}.zip
 
 # Adjust TOMCAT config
-cd $DIR/deployment
+cd $DEPLOYMENT
 curl -L -J -O https://github.com/rehei/tomcat7-rehei-xslt/releases/download/0.4.0/server.xslt
-SERVER_XML=$DIR/deployment/${TOMCAT}/conf/server.xml
+SERVER_XML=$DEPLOYMENT/${TOMCAT}/conf/server.xml
 saxon-xslt -o ${SERVER_XML} ${SERVER_XML} server.xslt port=${PORT}
 
 curl -L -J -O https://github.com/rehei/tomcat7-rehei-xslt/releases/download/0.4.0/context.xslt
-CONTEXT_XML=$DIR/deployment/${TOMCAT}/conf/context.xml
+CONTEXT_XML=$DEPLOYMENT/${TOMCAT}/conf/context.xml
 saxon-xslt -o ${CONTEXT_XML} ${CONTEXT_XML} context.xslt
 
 # Package everything into one zip file 
-cd $DIR/deployment
-zip -r $DIR/deployment/${TOMCAT}.zip ${TOMCAT}
+cd $DEPLOYMENT
+zip -r $DEPLOYMENT/${TOMCAT}.zip ${TOMCAT}
 
 echo "Done" 
 
